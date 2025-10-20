5eb32d7c8c61001e8f19feef6ffb3fb373ca8d5b
stjchang <changstephen289@gmail.com>
2025-10-20 01:58:22 -0400

    linked list def with append, length, display

--- personal/data-structures/LinkedList.py
+++ personal/data-structures/LinkedList.py
@@ -1,27 +1,43 @@
+class Node:
+    def __init__(self, data=None):
+            self.data = data
+            self.next = None
+                
+class LinkedList:
+    def __init__(self):
+        self._head = Node() # this head is a dummy node
+        self._tail = self._head
+    
+    def append(self, data):
+        dummy = Node(data)
+        self._tail.next = dummy
+        self._tail = dummy
+    
+    def length(self):
+        curr = self._head
+        res = 0
+        while curr.next:    
+            curr = curr.next
+            res += 1
+        
+        return res
+    
+    def display(self):
+        res = []
+        curr = self._head 
+        while curr.next:
+            curr = curr.next
+            res.append(curr.data)
 
             
-    
+        return res
 
-class LinkedList:
-    class Node:
-        def __init__(self, value, next=None):
-                self.value = value
-                self.next = next
-        
-        def get(self, index):
-            if index >= 0:
-                for _ in range(index):
-                    self = self.next
-                    if not self:
-                        break
-                return self 
-    
-    def __init__(self):
-        self._head = None
-        self._tail = None
-        self._length = 0
-    
-    def isempty(self):
-        return self._length == 0
+if __name__ == "__main__":
+    my_list = LinkedList()
+    my_list.append(1)
+    my_list.append(2)
+    my_list.append(3)
 
-        
+    print("List contents:", my_list.display())
+    print("List length:", my_list.length())
+    
